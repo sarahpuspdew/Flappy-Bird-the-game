42bca2dce8fd4429f10aacbea0812012002e900b
sarahpuspdew <sarahpuspadewi.spd@gmail.com>
2023-11-02 11:35:30 +0700

    Implement medal

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@export var bronze : Texture
+@export var platinum : Texture
+@export var gold : Texture
+
+@onready var last_best_score = SaveFile.best_score
+
 
 func _ready():
 	GameManager.game_started.connect(_on_game_started)
@@ -19,6 +25,11 @@ func _on_game_started():
 func _on_bird_crashed():
 	$Countdown.visible = false
 	$GameOver.visible = true
+	
+	$GameOver/Panel/Score.text = str(GameManager.score)
+	$GameOver/Panel/BestScore.text = str(SaveFile.best_score)
+	
+	show_medal()
 
 
 func _on_countdown_timer_timeout():
@@ -28,3 +39,12 @@ func _on_countdown_timer_timeout():
 func _on_play_button_pressed():
 	get_tree().reload_current_scene()
 	GameManager.is_game_started = false
+
+
+func show_medal():
+	if GameManager.score > last_best_score:
+		$GameOver/Panel/Medal.texture = gold
+	elif GameManager.score == SaveFile.best_score:
+		$GameOver/Panel/Medal.texture = platinum
+	elif GameManager.score < SaveFile.best_score:
+		$GameOver/Panel/Medal.texture = bronze
